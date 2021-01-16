f02c71191a059d45a812e06dba7fbf4dfddf23cb
xu Sun <ssnfee@gmail.com>
2021-01-16 22:59:42 +0800

    添加假期悬浮窗，未完

--- Itsycal/other/TooltipViewController.m
+++ Itsycal/other/TooltipViewController.m
@@ -8,6 +8,7 @@
 
 #import "TooltipViewController.h"
 #import "Themer.h"
+#import "SCUtils.h"
 
 @implementation TooltipViewController
 
@@ -16,6 +17,19 @@
     self.tv.enableHover = NO;
     self.tv.enclosingScrollView.hasVerticalScroller = NO; // in case user has System Prefs set to always show scroller
     self.events = [self.tooltipDelegate eventsForDate:date];
+    
+    NSString *todayDateStr = NSStringFromMoDateWithoutJulian(date);
+    KCNATIONSTATUS todayType = [SCUtils whichNationDays:todayDateStr];
+    NSNumber *typeNum = [NSNumber numberWithInteger:todayType];
+    if (todayType & (KCNATIONSTATUSrelax | KCNATIONSTATUSwork)) {
+        if (self.events) {
+            NSMutableArray *addTypeArr = [NSMutableArray arrayWithObject:typeNum];
+            [addTypeArr addObjectsFromArray:self.events];
+            self.events = addTypeArr;
+        } else {
+            self.events = [NSArray arrayWithObject:typeNum];
+        }
+    }
     if (self.events) {
         [self reloadData];
         return YES;
